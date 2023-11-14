1a9dacda0b3a2e7225ba5b10ec7e56e19a167d50
Heiko Köppen <heiko.koeppen@gmx.de>
2023-11-14 11:59:06 +0100

    centralized page management in steps

--- src/test/java/com/avenqo/medusa/fe/selenium/test/steps/StoreFrontendSteps.java
+++ src/test/java/com/avenqo/medusa/fe/selenium/test/steps/StoreFrontendSteps.java
@@ -41,7 +41,12 @@ public class StoreFrontendSteps {
 	private static final String STEP = "";
 	
 	private OrderConfirmationPage orderConfirmationPage = new OrderConfirmationPage(); 
-
+	private NavigationBar navigationBar = new NavigationBar();
+	private BasketPage basketPage = new BasketPage();
+	private CheckoutPage checkoutPage = new CheckoutPage();
+	private LandingPage landingPage = new LandingPage();
+	private ProductDetailsPage productDetailsPage = new ProductDetailsPage();
+	
 	@Given("ich bin ein unregistrierter Kunde")
 	public void unregisteredUser() {
 		LOG.debug(STEP);
@@ -51,51 +56,40 @@ public class StoreFrontendSteps {
 	@Given("ich lege das Produkt {string} in den Warenkorb")
 	public void ich_lege_das_produkt_in_den_warenkorb(String productName) {
 		LOG.debug(STEP);
-		LandingPage landingPage = new LandingPage();
+		
 		landingPage.selectProductByName(productName);
-
-		ProductDetailsPage productDetailsPage = new ProductDetailsPage();
 		productDetailsPage.waitUntilVisible();
 		productDetailsPage.addToCart();
-
-		NavigationBar navigationBar = new NavigationBar();
 		navigationBar.waitUntilBagIsContainingItems(1);
-
+		
 		assertEquals(1, navigationBar.getNumberOfArticlesInBasket(), "Number of articles in basket is wrong.");
 	}
 
 	@Given("ich gehe mit dem Warenkorb zur Kasse")
 	public void ich_gehe_mit_dem_Warenkorb_zur_kasse() {
 		LOG.debug(STEP);
-		NavigationBar navigationBar = new NavigationBar();
 		navigationBar.gotoBag();
-
-		BasketPage basketPage = new BasketPage();
 		basketPage.waitUntilVisible();
 		basketPage.checkout();
-
-		CheckoutPage checkoutPage = new CheckoutPage();
 		checkoutPage.waitUntilVisible();
-
 	}
 
 	@Given("ich gebe meine minimalen Versandinformationen ein")
 	public void ich_gebe_meine_minimalen_versandinformationen_ein() {
 		LOG.debug(STEP);
-		CheckoutPage checkoutPage = new CheckoutPage();
 		checkoutPage.enterMinimalAddressAndContinue(TestDataProvider.get().getCurrentCustomer());
 	}
 
 	@Given("ich wähle die Versandmethode {string}")
 	public void ich_wähle_die_versandmethode(String string) {
 		LOG.debug(STEP);
-		new CheckoutPage().selectDeliveryService(DELIVERY_STANDARD);
+		checkoutPage.selectDeliveryService(DELIVERY_STANDARD);
 	}
 
 	@When("ich das Produkt kaufe")
 	public void ich_das_produkt_kaufe() {
 		LOG.debug(STEP);
-		new CheckoutPage().pushCheckout();
+		checkoutPage.pushCheckout();
 	}
 
 	@Then("sehe ich die Bestellbestätigung")
@@ -112,7 +106,7 @@ public class StoreFrontendSteps {
 	@Then("die Bestellbestätigung enthält meine Kundendaten")
 	public void die_bestellbestätigung_enthält_meine_kundendaten() {
 		LOG.debug(STEP);
-		DeliveryAddress deliveryAddress = new OrderConfirmationPage().getDeliveryAddress();
+		DeliveryAddress deliveryAddress = orderConfirmationPage.getDeliveryAddress();
 		Customer currentCustomer = TestDataProvider.get().getCurrentCustomer();
 		assertEquals(currentCustomer.getFirstName() + " " + currentCustomer.getLastName(), deliveryAddress.getName());
 
@@ -125,9 +119,7 @@ public class StoreFrontendSteps {
 	@Then("die Bestellbestätigung enthält die Artikel")
 	public void die_bestellbestätigung_enthält_die_artikel(io.cucumber.datatable.DataTable dataTable) {
 		LOG.debug(STEP);
-
 		List<Map<String, String>> rows = dataTable.asMaps();
-		OrderConfirmationPage orderConfirmationPage = new OrderConfirmationPage();
 		assertEquals(rows.get(0).get("Artikel"), orderConfirmationPage.getArticle());
 		assertEquals(rows.get(0).get("Variante"), orderConfirmationPage.getVariant());
 		assertEquals(rows.get(0).get("Anzahl"), orderConfirmationPage.getCount());
@@ -137,10 +129,7 @@ public class StoreFrontendSteps {
 	@Then("die Bestellbestätigung enthält in der Zusammenfassung")
 	public void die_bestellbestätigung_enthält(io.cucumber.datatable.DataTable dataTable) {
 		LOG.debug(STEP);
-
 		Map<String, String> items = dataTable.asMap();
-
-		OrderConfirmationPage orderConfirmationPage = new OrderConfirmationPage();
 		assertEquals(items.get("Teilkosten"), orderConfirmationPage.getSubtotal());
 		assertEquals(items.get("Versandkosten"), orderConfirmationPage.getDeliveryPrice());
 		assertEquals(items.get("Gesamtkosten"), orderConfirmationPage.getTotal());
@@ -148,9 +137,7 @@ public class StoreFrontendSteps {
 
 	@Then("die Bezahlung der aktuellen Bestellung ist veranlasst")
 	public void die_bestellbestätigung_enthält_meine_zahlmethode(io.cucumber.datatable.DataTable dataTable) {
-		
 		LOG.debug(STEP);
-		
 		ValidatableResponse validatableResponse = new OrderApi().getOrderById(TestDataProvider.get().getCurrentOrder().getId());
 		
 		Map<String, String> items = dataTable.asMap();
@@ -161,5 +148,4 @@ public class StoreFrontendSteps {
 		validatableResponse.body("order.currency_code", equalTo(items.get("Währung")));
 		validatableResponse.body("order.payment_status", equalTo(items.get("Payment Status")));
 	}
-
 }
